166f31d1892dbb0b09211d0822f4e343b73dd977
OmarMontemayor <omarmontemayor11@yahoo.com>
2020-05-29 11:35:10 -0500

    Added API request method

--- lib/star_wars_library/api.rb
+++ lib/star_wars_library/api.rb
@@ -1,3 +1,14 @@
 class Api
-    puts "Api class loaded"
+    
+    BASE_URL = 'https://swapi.dev/api/'
+
+    def self.get_films
+        response = RestClient.get(BASE_URL + "films/")
+        data = JSON.parse(response.body)
+        data['results'].each do |film|
+            title = film['title']
+            episode_id = film['episode_id']
+            Film.new(title, episode_id)
+        end
+    end
 end
--- lib/star_wars_library/film.rb
+++ lib/star_wars_library/film.rb
@@ -1,3 +1,13 @@
 class Film
-    puts "Film class loaded"
+    attr_accessor :title, :episode_id
+    @@all = []
+    def initialize(title, episode_id)
+        @title = title
+        @episode_id = episode_id
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
